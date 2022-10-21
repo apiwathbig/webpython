691941a81a2ba6151dca7f26614b49d2162e0dc0
apiwathbig <113652300+apiwathbig@users.noreply.github.com>
2022-10-21 11:28:09 +0700

    .commit

--- Home.py
+++ Home.py
@@ -8,3 +8,13 @@ import plotly.graph_objects as go
 
 st.header("Apiwath")
 
+html_8="""
+<div style="background-color:#EE9513;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>เกณฑ์การให้คะแนน 4</h5></center>
+</div>
+"""
+st.markdown(html_8,unsafe_allow_html=True)
+st.markdown("")
+dx=pd.read_excel('./data/gen.xlsx')
+st.dataframe(dx)
+
