261c4350aad139ec8087020048ac2bfc6c8afb59
apiwathbig <113652300+apiwathbig@users.noreply.github.com>
2022-11-18 10:10:56 +0700

    .commit

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pickle
 
-st.image("./pic/2.jpg")
+st.image("./pic/2.png")
 
 html_8="""
 <div style="background-color:#EE9513;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
